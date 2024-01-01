644661dcb890f11b889bf08f27c6fb8cac6068fe
exit1100 <85146195+exit1100@users.noreply.github.com>
2024-01-01 21:39:08 +0900

    first Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JOONGSINT
+중부대학교 23-1 CCIT 팀 프로젝트
---
+++ config.py
@@ -0,0 +1,13 @@
+Instagram_ID = ''
+Instagram_PW = ''
+
+Facebook_ID = ''
+Facebook_PW = ''
+
+google_api_key=''
+google_cse_id=''
+naver_client_id=''
+naver_client_secret=''
+openai_api_key=''
+
+github_access_token = ""
